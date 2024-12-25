370ad16c74de2b13f254f27cdfa01fc945bf1e88
gmells <victormells30@gmail.com>
2024-12-25 23:45:09 +0000

    edit Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ user }: SiderbarProps) => {
+  return <section className="sidebar">Sidebar</section>;
 };
 
 export default Sidebar;
