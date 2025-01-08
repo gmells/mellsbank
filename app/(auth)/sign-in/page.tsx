c3249abb4373ed97e8c85837c216f11cb893fedd
gmells <victormells30@gmail.com>
2025-01-08 19:30:49 +0000

    center SignIn page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">SignIn</section>
+  );
 };
 
 export default SignIn;
