2b349e7a5ac5d1c8e3ae149796eb33e1fa8ed478
gmells <victormells30@gmail.com>
2025-01-09 22:26:06 +0000

    Add AuthForm component to SignIn page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <section className="flex-center size-full max-sm:px-6">SignIn</section>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm></AuthForm>
+    </section>
   );
 };
 
