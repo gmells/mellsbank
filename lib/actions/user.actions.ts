6f93992d560e8b85d7b62ac554878c1bf1db1718
gmells <victormells30@gmail.com>
2025-01-23 22:08:47 +0000

    add user action file

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+export const signIn = async () => {
+    try {
+        // Mutation / Modify Database / Fetch Data
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Mutation / Modify Database / Fetch Data
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
