b3c9f8d56d0520aa38cc24ce5d714a0e2aaf45c1
gmells <victormells30@gmail.com>
2025-01-26 19:37:39 +0000

    add sdk from appwrite to authenticate the user and fetch userData

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+
 export const signIn = async () => {
     try {
         // Mutation / Modify Database / Fetch Data
@@ -10,9 +13,37 @@ export const signIn = async () => {
 
 
 export const signUp = async (userData: SignUpParams) => {
+    const {email password, firstName, lastName} = userData
     try {
-        // Mutation / Modify Database / Fetch Data
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+        );
+        const session = await account.createEmailPasswordSession(email, password);
+      
+        cookies().set("my-custom-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
     } catch (error) {
         console.log('Error', error)
     }
 }
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
