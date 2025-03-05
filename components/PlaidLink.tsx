d8f24e31d5a7f7ef018e0b0ca876c2acebaba16e
gmells <victormells30@gmail.com>
2025-03-05 17:45:21 +0000

    complete PlaidLink component

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link";
+import { StyledString } from "next/dist/build/swc/types";
+import { useRouter } from "next/navigation";
 
-const PlaidLink = ({ user }: PlaidLinkProps) => {
-  return <div>PlaidLink</div>;
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      // const data = await createLinkToken(user);
+      // setToken(data?.LinkToken);
+    };
+
+    getLinkToken();
+  }, []);
+
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
+      // await exchangePublicToken({ publicToken: public_token, user });
+
+      router.push("/");
+    },
+    [user]
+  );
+
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+
+  return (
+    <>
+      {variant === "primary" ? (
+        <Button
+          onClick={() => open()}
+          disabled={!ready}
+          className="plaidlink-primary"
+        >
+          {" "}
+          Connect bank{" "}
+        </Button>
+      ) : variant === "ghost" ? (
+        <Button> Connect bank </Button>
+      ) : (
+        <Button> Connect bank </Button>
+      )}{" "}
+    </>
+  );
 };
 
 export default PlaidLink;
