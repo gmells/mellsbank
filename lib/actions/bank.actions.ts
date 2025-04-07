bac40630330fa8361c69089c0ca2858ef7e84a2f
gmells <victormells30@gmail.com>
2025-04-07 22:53:55 +0100

    temporarily load only first account transactions

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -162,7 +162,7 @@ export const getTransactions = async ({
  
   try {
     // Iterate through each page of new transaction updates for item
-    while (hasMore) {
+    //while (hasMore) {
       const response = await plaidClient.transactionsSync({
         access_token: accessToken
       });
@@ -187,7 +187,7 @@ export const getTransactions = async ({
       }));
 
       hasMore = data.has_more;
-    }
+    //}
 
    
     return parseStringify(transactions);
