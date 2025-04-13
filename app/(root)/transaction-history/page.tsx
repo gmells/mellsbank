f2a4ea65b63efd6064fe46f39bab7a0cab080b02
gmells <victormells30@gmail.com>
2025-04-13 23:42:43 +0100

    Add pagination functionality in transaction history pagge

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import { Pagination } from "@/components/Pagination";
 import TransactionsTable from "@/components/TransactionsTable";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
@@ -18,6 +19,18 @@ const TransactionHistory = async ({
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId });
+
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexofFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(
+    indexofFirstTransaction,
+    indexOfLastTransaction
+  );
+
   return (
     <div className="transactions">
       <div className="transactions-header">
@@ -49,6 +62,12 @@ const TransactionHistory = async ({
 
         <section className="flex w-full flex-col gap-6">
           <TransactionsTable transactions={account?.transactions} />
+
+          {totalPages > 1 && (
+            <div className="my-4 w-full">
+              <Pagination totalPages={totalPages} page={currentPage} />
+            </div>
+          )}
         </section>
       </div>
     </div>
