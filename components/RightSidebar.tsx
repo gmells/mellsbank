6525fe3f0d4c8067d3b10c3ddba9e3da6183baa0
gmells <victormells30@gmail.com>
2025-04-17 23:55:13 +0100

    style categories table

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -55,6 +55,11 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             )}
           </div>
         )}
+
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+          <div className="space-y-5">{/* Categories */}</div>
+        </div>
       </section>
     </aside>
   );
