f97ebdb57369b1f6e929b390540671637e02f694
gmells <victormells30@gmail.com>
2025-04-19 23:24:22 +0100

    map category

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -63,7 +63,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="mt-10 flex flex-1 flex-col gap-6">
           <h2 className="header-2">Top categories</h2>
           <div className="space-y-5">
-            {Categories.map((category, index) => (
+            {categories.map((category, index) => (
               <Category key={category.name} category={category} />
             ))}
           </div>
