33f26b62ac67bbbcb13586224109265c1c31b531
gmells <victormells30@gmail.com>
2025-04-20 23:02:06 +0100

    correct error with searchParams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,13 @@ import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: { id: string; page: string };
+}) => {
+  // Wait for searchParams before accessing
+  const { id, page } = await searchParams;
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id });
